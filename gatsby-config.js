60190fe1f8e69111284f2efbe2671881197508a2
Alex Marmon <alex@14four.com>
2018-06-05 15:00:41 -0700

    analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,18 @@ module.exports = {
           'Raleway\:500'  // eslint-disable-line
         ]
       }
+    },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-120428398-1',
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true
+      },
     }
   ],
 }
