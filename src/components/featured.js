65b96182b0144274cf22954f754e4c97a5086524
Alex Marmon <aemarmon@gmail.com>
2019-12-06 14:29:34 -0800

    add dutchie

--- src/components/featured.js
+++ src/components/featured.js
@@ -3,6 +3,34 @@ import styled from 'styled-components'
 import styles, { MainContainer, media } from '../styles'
 import arrow from '../assets/images/arrow.svg'
 
+const PlayButtonContainer = styled.div`
+top: 50%; right: 50%;
+transform: translate(50%,-50%);
+  position: absolute;
+  z-index: 100;
+  cursor: pointer;
+
+  ${media.largePhone`
+    top: 50%;
+    left: calc(50% - 29px);
+  `}
+`
+
+const PlayButton = () => (
+  <PlayButtonContainer onClick={() => window.open('https://vimeo.com/372983870', '_blank')}>
+    <svg xmlns="http://www.w3.org/2000/svg" width={115} height={115} viewBox="0 0 115 115">
+      <g fill="none" fillRule="evenodd" transform="translate(.6 .85)">
+        <circle cx="57" cy="57" r="57" fill="#27BD82" />
+        <circle cx="57" cy="57" r="49.4" fill="#27BD82" />
+        <path
+          fill="#fff"
+          d="M41.8 40.364v32.96c.085 4.042 3.925 6.495 7.282 4.57L76.058 61.36c1.47-.903 2.475-2.56 2.475-4.499 0-1.94-1.005-3.596-2.475-4.499L49.082 35.83c-.839-.481-1.715-.7-2.547-.679-2.498.06-4.67 2.182-4.735 5.213z"
+        />
+      </g>
+    </svg>
+  </PlayButtonContainer>
+)
+
 const Featured = ({ data: { featured } }) => (  // eslint-disable-line
   <FeaturedStyles>
     <MainContainer>
@@ -11,7 +39,10 @@ const Featured = ({ data: { featured } }) => (  // eslint-disable-line
           <img className="logo" src={require(`../assets/images/${featured.logo}`)} alt="" />
           <p>{featured.copy}</p>
         </div>
-        <img className="main-image" src={require(`../assets/images/${featured.image}`)} alt="" />
+        <ImageContainer>
+          <img className="main-image" src={require(`../assets/images/${featured.image}`)} alt="" />
+          <PlayButton />
+        </ImageContainer>
         <a href={featured.link} target="_blank" className="arrow-container">
           <img className="arrow" src={arrow} alt="" />
         </a>
@@ -29,6 +60,11 @@ export const query = graphql`
       link
     }
   }
+`
+
+const ImageContainer = styled.div`
+  height: 80%;
+  position: relative;
 `
 
 const FeaturedStyles = styled.div.attrs({ className: 'featured-styles' })`
@@ -60,7 +96,7 @@ const FeaturedStyles = styled.div.attrs({ className: 'featured-styles' })`
 
   img.logo {
     max-width: 122px;
-    width: 8vw;
+    width: 3vw;
   }
 
   p {
@@ -69,7 +105,7 @@ const FeaturedStyles = styled.div.attrs({ className: 'featured-styles' })`
   }
 
   img.main-image {
-    height: 80%;
+    height: 100%;
   }
 
   .arrow-container {
